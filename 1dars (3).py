b3e2bcc3da84d569cbf512f277a7b22fe2685736
Brave-cyber-security <tulqinovbotirbek480@gmail.com>
2024-10-29 17:26:49 +0500

    Add files via upload

---
+++ 1dars (3).py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, first_name, last_name, age, card_number, student):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.card_number = card_number
+        self.student = student
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    @property
+    def about(self):
+        return f"I am {self.full_name}, I am {self.student}, I am {self.age}, I have {self.card_number} numbered in the student card"
+student = Student('Botirbek', 'To\'lqinov', 18, 245876, 'student')
+print(student.about)
